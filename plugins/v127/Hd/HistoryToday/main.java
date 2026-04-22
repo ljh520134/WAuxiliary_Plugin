c046d8afc106bd11135394f9bd0ebe1720022b78
hd <hdshare@vip.qq.com>
2026-04-22 09:23:48 +0800

    优化: 历史今天插件

---
+++ plugins/v127/Hd/HistoryToday/main.java
@@ -0,0 +1,28 @@
+
+import me.hd.wauxv.plugin.api.callback.PluginCallBack;
+
+void sendToday(String talker) {
+    var api = "https://v2.xxapi.cn/api/historypic";
+    get(api, null, (respCode, respContent) -> {
+        var jsonObj = new JSONObject(respContent);
+        var code = jsonObj.optInt("code");
+        if (code == 200) {
+            var url = jsonObj.optString("data");
+            var path = "${cacheDir}/image.png";
+            download(url, path, null, cacheFile -> {
+                sendImage(talker, cacheFile.getAbsolutePath());
+                cacheFile.delete();
+            });
+        }
+    });
+}
+
+void onHandleMsg(Object msgInfoBean) {
+    if (msgInfoBean.isText()) {
+        var content = msgInfoBean.getContent();
+        var talker = msgInfoBean.getTalker();
+        if (content.equals("/历史今天")) {
+            sendToday(talker);
+        }
+    }
+}
